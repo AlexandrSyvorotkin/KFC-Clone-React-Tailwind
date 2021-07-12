eefbed00c10b71e90c7f4bac180511cd64928375
Alex <Aleks031994@Mail.ru>
2021-07-12 14:17:35 +0300

    styling header section

--- src/components/Layout/Header/Header.jsx
+++ src/components/Layout/Header/Header.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from './Header.module.css'
 import HumburgerMenu from "./HumburgerMenu";
 import HeaderButton from "./HeaderButton";
+import HeaderForm from "./HeaderForm/HeaderForm";
 
 const Header = () => {
+
+    const [foodDelivery, setFoodDelivery] = useState(false)
+
+    const startGetDelivery = () => {
+        setFoodDelivery(true)
+    }
+    const getPickUp = () => {setFoodDelivery(false)}
+
     return (
         <>
             <div className={styles.header}>
@@ -18,10 +27,11 @@ const Header = () => {
             <div className={styles.header_info}>
                 <h1>Наш адрес: г. Москва ул. Театральный пр-д, 5/1</h1>
                 <div>
-                    <button>Доставка</button>
-                    <button>Самовывоз</button>
+                    <button className={styles.btn} onClick={startGetDelivery}>Доставка</button>
+                    <button className={styles.btn} onClick={getPickUp}>Самовывоз</button>
                 </div>
             </div>
+            {foodDelivery && <HeaderForm/>}
         </>
     );
 };
--- src/components/Layout/Header/HeaderForm/HeaderForm.jsx
+++ src/components/Layout/Header/HeaderForm/HeaderForm.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import styles from './HeaderForm.module.css'
 
 const HeaderForm = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+            <div className={styles.city_adress}>Доставка город Москва</div>
+            <form className={styles.form}>
+                <div className={styles.input}>
+                    <label htmlFor="street">Улица</label>
+                    <input  type='text' placeholder="Остоженка"/>
+                </div>
+                <div className={styles.input}>
+                    <label htmlFor="house">Дом</label>
+                    <input type='number' placeholder='8'/>
+                </div>
+            </form>
+        </>
     );
 };
 
