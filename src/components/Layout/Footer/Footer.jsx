6f2580984f1ad931a986bed72157fc76c3b743d6
Alex <Aleks031994@Mail.ru>
2021-07-12 17:28:40 +0300

    and footer component and styles

--- src/components/Layout/Footer/Footer.jsx
+++ src/components/Layout/Footer/Footer.jsx
@@ -2,36 +2,42 @@ import React from 'react';
 import styles from './Footer.module.css'
 import FooterInfoLinks from "./FooterInfoLinks";
 
+import VK from '../../assets/vk.png'
+import FaceBook from '../../assets/facebook.png'
+import Twitter from '../../assets/twitter.png'
+import Classmates from '../../assets/classmates.png'
+import GoogleStore from '../../assets/GooglePlay.png'
+import AppleStore from '../../assets/Appstore.png'
+
 const Footer = () => {
 
-    const companyLinks = [
-        'Компания',
-        'Новости',
-        'Экскурсии',
-        'Бизнес c RFC',
-        'RSC Battle',
-        'Политика обработки и защиты ПДн',
-        'Составы блюд']
-
-    const careerLinks = [
-        'Университет',
-        'Вакансии',
-        'Анкета']
-
-    const socialLinks  = [
-        'Вконтакте',
-        'Фэйсбук',
-        'Инстаграмм',
-        'Одноклассники',
-        'Ютуб'
-    ]
-
-    const ContactLinks = [
-        'Обратная связь',
-        'Для прессы',
-        'Сотрудникам'
-    ]
-
+    const Links = {
+        companyLinks: [
+            'Компания',
+            'Новости',
+            'Экскурсии',
+            'Бизнес c RFC',
+            'RSC Battle',
+            'Составы блюд'
+        ],
+        careerLinks: [
+            'Университет',
+            'Вакансии',
+            'Анкета'
+        ],
+        socialLinks: [
+            'Вконтакте',
+            'Фэйсбук',
+            'Инстаграмм',
+            'Одноклассники',
+            'Ютуб'
+        ],
+        ContactLinks: [
+            'Обратная связь',
+            'Для прессы',
+            'Сотрудникам'
+        ]
+    }
 
     return (
         <footer className={styles.footer}>
@@ -43,11 +49,31 @@ const Footer = () => {
                 </div>
             </div>
             <div className={styles.footer_sections}>
-                <FooterInfoLinks links={companyLinks} title='Компания'/>
-                <FooterInfoLinks links={careerLinks} title='Карьера'/>
+                <FooterInfoLinks links={Links.companyLinks} title='Компания'/>
+                <FooterInfoLinks links={Links.careerLinks} title='Карьера'/>
                 <img className={styles.rfc_logo} src="https://energytorg.ru/images/1024px-KFC_logo.svg.png" alt="KFC"/>
-                <FooterInfoLinks links={socialLinks} title='Мы рядом'/>
-                <FooterInfoLinks links={ContactLinks} title='Контакты'/>
+                <FooterInfoLinks links={Links.socialLinks} title='Мы рядом'/>
+                <FooterInfoLinks links={Links.ContactLinks} title='Контакты'/>
+            </div>
+            <div className={styles.footer_end}>
+                <div className={styles.footer_icon_links}>
+                    <a href="https://vk.com/kfcrussia">
+                        <img src={VK} alt="vk.com"/>
+                    </a>
+                    <a href="https://www.facebook.com/kfcrussia">
+                        <img src={FaceBook} alt="facebook.com"/>
+                    </a>
+                    <a href="https://twitter.com/kfcrussia">
+                        <img src={Twitter} alt="twitter.com"/>
+                    </a>
+                    <a href="https://ok.ru/group/53854456184833">
+                        <img src={Classmates} alt="ok.ru"/>
+                    </a>
+                </div>
+                <div className={styles.footer_stores}>
+                    <img src={GoogleStore} alt=""/>
+                    <img src={AppleStore} alt=""/>
+                </div>
             </div>
         </footer>
     );
