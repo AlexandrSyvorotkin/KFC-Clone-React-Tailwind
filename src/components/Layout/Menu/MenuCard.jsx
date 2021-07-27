278159ae9650797b4317f70fb78d6818e3f2d43a
Alex <Aleks031994@Mail.ru>
2021-07-27 20:03:53 +0300

    big implementations to card component to style it correctly

--- src/components/Layout/Menu/MenuCard.jsx
+++ src/components/Layout/Menu/MenuCard.jsx
@@ -16,15 +16,14 @@ const MenuCard = ({img, price, name, hit, newProduct}) => {
     }
 
     return (
-        <div className='flex flex-col justify-between'>
-            <div className='relative mb-1'>
+        <div className='flex flex-col justify-between relative mr-6'>
+            <div>
                 {newProduct ? <div className='productStatus bg-red-500 '>{newProduct}</div>
                     : null
                 }
                 {hit ? <div className='productStatus bg-black'>{hit}</div>
                     : null
                 }
-                <img src={img} alt=""/>
                 {
                     !amountOfMeal ? <div className='amountMeals justify-between w-28 bg-black '>
                         <div onClick={hideAmountOfMealHandler}><img className='px-4' src={Minus} alt=""/></div>
@@ -37,6 +36,9 @@ const MenuCard = ({img, price, name, hit, newProduct}) => {
                         <div><img src={BlackPlus} alt=""/></div>
                     </div>
                 }
+            </div>
+            <div className='mb-1 h-80 flex items-center justify-center'>
+                <img className='object-cover max-h-full' src={img} alt=""/>
             </div>
             <div className='text-gray-500 text-center'>{name}</div>
             <div className='text-red-600 text-center'>{price}р.</div>
