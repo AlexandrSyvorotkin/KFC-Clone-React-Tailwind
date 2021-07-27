47a1f7b24c0983910cf9c2e190240cc028909b25
Alex <Aleks031994@Mail.ru>
2021-07-27 13:51:36 +0300

    implemented condition rendering to card buttons

--- src/components/Layout/Menu/MenuCard.jsx
+++ src/components/Layout/Menu/MenuCard.jsx
@@ -1,24 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Plus from '../../../assets/Plus.png'
 import Minus from '../../../assets/Minus.png'
 import BlackPlus from '../../../assets/blackPlus.jpg'
 
 const MenuCard = ({img, price, name}) => {
+
+    const [amountOfMeal, setAmountOfMeal] = useState(true)
+
+    const setAmountOfMealHandler = () => {
+        setAmountOfMeal(false)
+    }
+
     return (
         <div className='flex flex-col mr-5'>
             <div className='relative mb-1'>
                 <img src={img} alt=""/>
-                <div className='flex items-center justify-center absolute w-12 h-12 bg-white rounded-full bottom-0 right-0 shadow-lg'>
-                    <div><img src={BlackPlus} alt=""/></div>
-                </div>
+                {
+                    !amountOfMeal ? <div className='bg-black rounded-full w-28 h-12 flex items-center justify-between'>
+                        <img className='px-4' src={Minus} alt=""/>
+                        <div className='text-gray-50'>1</div>
+                        <img className='px-4' src={Plus} alt=""/>
+                    </div> : <div
+                        className='flex items-center justify-center absolute w-12 h-12 bg-white rounded-full bottom-0 right-0 shadow-lg'
+                        onClick={setAmountOfMealHandler}
+                    >
+                        <div><img src={BlackPlus} alt=""/></div>
+                    </div>
+                }
             </div>
             <div className='text-gray-500 text-center'>{name}</div>
             <div className='text-red-600 text-center'>{price}р.</div>
-            <div className='bg-black rounded-full w-28 h-12 flex items-center justify-between'>
-                <img className='px-4' src={Minus} alt=""/>
-                <div className='text-gray-50'>1</div>
-                <img className='px-4' src={Plus} alt=""/>
-            </div>
         </div>
     );
 };
